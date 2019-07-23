f0e1e8ceacd48411b6717fd5e62c36bab4f2df3a
JosefSchmidt <josef.brondum.schmidt@gmail.com>
2019-07-23 17:00:25 +0200

    Log in function works now

--- src/app/main/login/Login.js
+++ src/app/main/login/Login.js
@@ -1,23 +1,20 @@
 import React, {Component} from 'react';
-import {Button, Card, CardContent, Checkbox, Divider, FormControl, FormControlLabel, TextField, Typography} from '@material-ui/core';
-import {makeStyles} from '@material-ui/styles';
-import {darken} from '@material-ui/core/styles/colorManipulator';
+import {
+    Button,
+    Card,
+    CardContent,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    TextField,
+    Typography
+} from '@material-ui/core';
 import {FuseAnimate} from '@fuse';
-import {useForm} from '@fuse/hooks';
-import {Link} from 'react-router-dom';
-import clsx from 'clsx';
+import {Link, Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import Image from './tjenerteam2.jpg'
 import * as user from '../../auth/store/actions/login.actions'
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        backgroundImage: `url(${Image})`,
-        background: 'linear-gradient(to right, ' + theme.palette.primary.dark + ' 0%, ' + darken(theme.palette.primary.dark, 0.5) + ' 100%)',
-        color     : theme.palette.primary.contrastText
-    }
-}));
 
 class Login extends Component {
     constructor(props) {
@@ -30,17 +27,14 @@ class Login extends Component {
     }
 
 
-    isFormValid = () => {
-        return (
-            this.state.email.length > 0 &&
-            this.state.password.length > 0
-        );
+    dd = (e) => {
+        let tempState = {...this.state.corporation};
+        tempState[e.target.name] = e.target.value;
+        this.setState(tempState);
     };
 
-    onChange = (e) => {
-        const tempState = {...this.state};
-        tempState[e.target.name] = e.target.value;
-        this.setState({tempState})
+    isFormValid = () => {
+        return (this.state.email.length > 0 && this.state.password.length > 0);
     };
 
     handleSubmit = () => {
@@ -49,44 +43,50 @@ class Login extends Component {
     };
 
     render() {
-        const classes = useStyles();
         const {email, password, remember} = this.state;
 
-    return (
+        if (this.props.success) return <Redirect to="/homeCorporation"/>;
 
-        <div style={{width: '100%'}}
-             className={clsx(classes.root, "flex flex-col flex-auto flex-shrink-0 p-16 md:flex-row md:p-0")}>
+        return (
+            <div style={{
+                width: '100%',
+                backgroundImage: "url(" + Image + ")",
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+            }} className="flex flex-col flex-auto flex-shrink-0 p-16 md:flex-row md:p-0">
 
 
-            <div
-                className="flex flex-col flex-grow-0 items-cent er text-white p-16 text-center md:p-128 md:items-start md:flex-shrink-0 md:flex-1 md:text-left">
+                <div
+                    className="flex flex-col flex-grow-0 items-cent er text-white p-16 text-center md:p-128 md:items-start md:flex-shrink-0 md:flex-1 md:text-left">
 
-                {/*<FuseAnimate animation="transition.expandIn">*/}
-                {/*<img className="w-128 mb-32" src="assets/images/logos/fuse.svg" alt="logo"/>*/}
-                {/*</FuseAnimate>*/}
+                    {/*<FuseAnimate animation="transition.expandIn">*/}
+                    {/*<img className="w-128 mb-32" src="assets/images/logos/fuse.svg" alt="logo"/>*/}
+                    {/*</FuseAnimate>*/}
 
-                <FuseAnimate animation="transition.slideUpIn" delay={300}>
-                    <Typography variant="h3" color="inherit" className="font-light">
-                        Velkommen til Tjenerteamet
-                    </Typography>
-                </FuseAnimate>
+                    <FuseAnimate animation="transition.slideUpIn" delay={300}>
+                        <Typography variant="h3" color="inherit" className="font-light">
+                            Velkommen til Tjenerteamet
+                        </Typography>
+                    </FuseAnimate>
 
-                <FuseAnimate delay={400}>
-                    <Typography variant="subtitle1" color="inherit" className="max-w-512 mt-16">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper nisl erat, vel
-                        convallis elit fermentum pellentesque. Sed mollis velit
-                        facilisis facilisis.
-                    </Typography>
-                </FuseAnimate>
-            </div>
+                    <FuseAnimate delay={400}>
+                        <Typography variant="subtitle1" color="inherit" className="max-w-512 mt-16">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper nisl erat,
+                            vel
+                            convallis elit fermentum pellentesque. Sed mollis velit
+                            facilisis facilisis.
+                        </Typography>
+                    </FuseAnimate>
+                </div>
 
-            <FuseAnimate animation={{translateX: [0, '100%']}}>
+                <FuseAnimate animation={{translateX: [0, '100%']}}>
 
-                <Card className="w-full max-w-640 mx-auto m-16 md:m-0" square>
+                    <Card className="w-full max-w-640 mx-auto m-16 md:m-0" square>
 
-                    <CardContent className="flex flex-col items-center p-32 md:p-128 md:pt-128 ">
+                        <CardContent className="flex flex-col items-center p-32 md:p-128 md:pt-128 ">
 
-                        <Typography variant="h6" className="md:w-full mb-32 ml-288">Log ind</Typography>
+                            <Typography variant="h6" className="md:w-full mb-32 ml-288">Log ind</Typography>
 
                             <TextField
                                 className="mb-16"
@@ -95,7 +95,7 @@ class Login extends Component {
                                 type="email"
                                 name="email"
                                 value={email}
-                                onChange={this.onChange}
+                                onChange={this.dd}
                                 variant="outlined"
                                 required
                                 fullWidth
@@ -107,7 +107,7 @@ class Login extends Component {
                                 type="password"
                                 name="password"
                                 value={password}
-                                onChange={this.onChange}
+                                onChange={this.dd}
                                 variant="outlined"
                                 required
                                 fullWidth
@@ -121,7 +121,7 @@ class Login extends Component {
                                             <Checkbox
                                                 name="remember"
                                                 value={remember}
-                                                onChange={this.onChange}/>
+                                                onChange={this.dd}/>
                                         }
                                         label="Husk mig"
                                     />
@@ -142,38 +142,38 @@ class Login extends Component {
                             >
                                 LOGIN
                             </Button>
-                        {/*<div className="my-24 flex items-center justify-center">*/}
-                        {/*    <Divider className="w-32"/>*/}
-                        {/*    <span className="mx-8 font-bold">ELLER</span>*/}
-                        {/*    <Divider className="w-32"/>*/}
-                        {/*</div>*/}
+                            {/*<div className="my-24 flex items-center justify-center">*/}
+                            {/*    <Divider className="w-32"/>*/}
+                            {/*    <span className="mx-8 font-bold">ELLER</span>*/}
+                            {/*    <Divider className="w-32"/>*/}
+                            {/*</div>*/}
 
-                        {/*<Button variant="contained" color="secondary" size="small"*/}
-                        {/*        className="normal-case w-192 mb-8">*/}
-                        {/*    Log ind med Google*/}
-                        {/*</Button>*/}
+                            {/*<Button variant="contained" color="secondary" size="small"*/}
+                            {/*        className="normal-case w-192 mb-8">*/}
+                            {/*    Log ind med Google*/}
+                            {/*</Button>*/}
 
-                        {/*<Button variant="contained" color="primary" size="small"*/}
-                        {/*        className="normal-case w-192">*/}
-                        {/*    Log ind med Facebook*/}
-                        {/*</Button>*/}
+                            {/*<Button variant="contained" color="primary" size="small"*/}
+                            {/*        className="normal-case w-192">*/}
+                            {/*    Log ind med Facebook*/}
+                            {/*</Button>*/}
 
-                        <div className="flex flex-col items-center justify-center pt-32 pb-24">
-                            <span className="font-medium">Har du ikke en profil endnu?</span>
-                            <Link className="font-medium" to="/createCorporation">Opret ny profil </Link>
-                        </div>
+                            <div className="flex flex-col items-center justify-center pt-32 pb-24">
+                                <span className="font-medium">Har du ikke en profil endnu?</span>
+                                <Link className="font-medium" to="/createCorporation">Opret ny profil </Link>
+                            </div>
 
-                    </CardContent>
-                </Card>
-            </FuseAnimate>
-        </div>
-    );
-}
+                        </CardContent>
+                    </Card>
+                </FuseAnimate>
+            </div>
+        );
+    }
 }
 
 function mapStateToProps(state) {
     return {
-        success: state.success
+        success: state.auth.login.success
     }
 }
 
@@ -186,8 +186,7 @@ function mapDispatchToProps(dispatch) {
 }
 
 
-export default
-connect(
+export default connect(
     mapStateToProps,
     mapDispatchToProps
 )
